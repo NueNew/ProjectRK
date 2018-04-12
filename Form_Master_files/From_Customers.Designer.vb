30014f5e5942d61643e809104aeda1626b085b9d
lockheath <34006723+lockheath@users.noreply.github.com>
2018-04-12 20:22:34 +0700

    ทำ form Customers
    master form customers
    add insert update delete
    binding navigator.

---
+++ Form_Master_files/From_Customers.Designer.vb
@@ -0,0 +1,381 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class From_Customers
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(From_Customers))
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.TextAddress = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.TextPhone = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextName = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.BindingNavigator1 = New System.Windows.Forms.BindingNavigator(Me.components)
+        Me.BindingNavigatorAddNewItem = New System.Windows.Forms.ToolStripButton()
+        Me.BindingNavigatorCountItem = New System.Windows.Forms.ToolStripLabel()
+        Me.BindingNavigatorDeleteItem = New System.Windows.Forms.ToolStripButton()
+        Me.BindingNavigatorMoveFirstItem = New System.Windows.Forms.ToolStripButton()
+        Me.BindingNavigatorMovePreviousItem = New System.Windows.Forms.ToolStripButton()
+        Me.BindingNavigatorSeparator = New System.Windows.Forms.ToolStripSeparator()
+        Me.BindingNavigatorPositionItem = New System.Windows.Forms.ToolStripTextBox()
+        Me.BindingNavigatorSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.BindingNavigatorMoveNextItem = New System.Windows.Forms.ToolStripButton()
+        Me.BindingNavigatorMoveLastItem = New System.Windows.Forms.ToolStripButton()
+        Me.BindingNavigatorSeparator2 = New System.Windows.Forms.ToolStripSeparator()
+        Me.SaveToolStripButton = New System.Windows.Forms.ToolStripButton()
+        Me.TextID = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.ButtonOK = New System.Windows.Forms.Button()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.TextSearch = New System.Windows.Forms.TextBox()
+        CType(Me.BindingNavigator1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.BindingNavigator1.SuspendLayout()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label8
+        '
+        Me.Label8.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Label8.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Label8.Location = New System.Drawing.Point(0, 112)
+        Me.Label8.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(1008, 4)
+        Me.Label8.TabIndex = 67
+        '
+        'TextAddress
+        '
+        Me.TextAddress.Location = New System.Drawing.Point(261, 341)
+        Me.TextAddress.Margin = New System.Windows.Forms.Padding(6, 6, 6, 6)
+        Me.TextAddress.Multiline = True
+        Me.TextAddress.Name = "TextAddress"
+        Me.TextAddress.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TextAddress.Size = New System.Drawing.Size(571, 176)
+        Me.TextAddress.TabIndex = 66
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(191, 341)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(41, 24)
+        Me.Label5.TabIndex = 65
+        Me.Label5.Text = "ที่อยู่"
+        '
+        'TextPhone
+        '
+        Me.TextPhone.Location = New System.Drawing.Point(261, 276)
+        Me.TextPhone.Margin = New System.Windows.Forms.Padding(6, 6, 6, 6)
+        Me.TextPhone.Name = "TextPhone"
+        Me.TextPhone.Size = New System.Drawing.Size(435, 29)
+        Me.TextPhone.TabIndex = 62
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(160, 274)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(72, 24)
+        Me.Label3.TabIndex = 61
+        Me.Label3.Text = "โทรศัพท์"
+        '
+        'TextName
+        '
+        Me.TextName.Location = New System.Drawing.Point(259, 202)
+        Me.TextName.Margin = New System.Windows.Forms.Padding(6, 6, 6, 6)
+        Me.TextName.Name = "TextName"
+        Me.TextName.Size = New System.Drawing.Size(437, 29)
+        Me.TextName.TabIndex = 60
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(164, 202)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(88, 24)
+        Me.Label2.TabIndex = 59
+        Me.Label2.Text = "ชื่อผู้บริจาค"
+        '
+        'BindingNavigator1
+        '
+        Me.BindingNavigator1.AddNewItem = Me.BindingNavigatorAddNewItem
+        Me.BindingNavigator1.BackColor = System.Drawing.SystemColors.ScrollBar
+        Me.BindingNavigator1.CountItem = Me.BindingNavigatorCountItem
+        Me.BindingNavigator1.DeleteItem = Me.BindingNavigatorDeleteItem
+        Me.BindingNavigator1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.BindingNavigatorMoveFirstItem, Me.BindingNavigatorMovePreviousItem, Me.BindingNavigatorSeparator, Me.BindingNavigatorPositionItem, Me.BindingNavigatorCountItem, Me.BindingNavigatorSeparator1, Me.BindingNavigatorMoveNextItem, Me.BindingNavigatorMoveLastItem, Me.BindingNavigatorSeparator2, Me.BindingNavigatorAddNewItem, Me.BindingNavigatorDeleteItem, Me.SaveToolStripButton})
+        Me.BindingNavigator1.Location = New System.Drawing.Point(0, 87)
+        Me.BindingNavigator1.MoveFirstItem = Me.BindingNavigatorMoveFirstItem
+        Me.BindingNavigator1.MoveLastItem = Me.BindingNavigatorMoveLastItem
+        Me.BindingNavigator1.MoveNextItem = Me.BindingNavigatorMoveNextItem
+        Me.BindingNavigator1.MovePreviousItem = Me.BindingNavigatorMovePreviousItem
+        Me.BindingNavigator1.Name = "BindingNavigator1"
+        Me.BindingNavigator1.Padding = New System.Windows.Forms.Padding(0, 0, 2, 0)
+        Me.BindingNavigator1.PositionItem = Me.BindingNavigatorPositionItem
+        Me.BindingNavigator1.Size = New System.Drawing.Size(1008, 25)
+        Me.BindingNavigator1.TabIndex = 58
+        Me.BindingNavigator1.Text = "BindingNavigator1"
+        '
+        'BindingNavigatorAddNewItem
+        '
+        Me.BindingNavigatorAddNewItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BindingNavigatorAddNewItem.Image = CType(resources.GetObject("BindingNavigatorAddNewItem.Image"), System.Drawing.Image)
+        Me.BindingNavigatorAddNewItem.Name = "BindingNavigatorAddNewItem"
+        Me.BindingNavigatorAddNewItem.RightToLeftAutoMirrorImage = True
+        Me.BindingNavigatorAddNewItem.Size = New System.Drawing.Size(23, 22)
+        Me.BindingNavigatorAddNewItem.Text = "Add new"
+        '
+        'BindingNavigatorCountItem
+        '
+        Me.BindingNavigatorCountItem.Name = "BindingNavigatorCountItem"
+        Me.BindingNavigatorCountItem.Size = New System.Drawing.Size(35, 22)
+        Me.BindingNavigatorCountItem.Text = "of {0}"
+        Me.BindingNavigatorCountItem.ToolTipText = "Total number of items"
+        '
+        'BindingNavigatorDeleteItem
+        '
+        Me.BindingNavigatorDeleteItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BindingNavigatorDeleteItem.Image = CType(resources.GetObject("BindingNavigatorDeleteItem.Image"), System.Drawing.Image)
+        Me.BindingNavigatorDeleteItem.Name = "BindingNavigatorDeleteItem"
+        Me.BindingNavigatorDeleteItem.RightToLeftAutoMirrorImage = True
+        Me.BindingNavigatorDeleteItem.Size = New System.Drawing.Size(23, 22)
+        Me.BindingNavigatorDeleteItem.Text = "Delete"
+        '
+        'BindingNavigatorMoveFirstItem
+        '
+        Me.BindingNavigatorMoveFirstItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BindingNavigatorMoveFirstItem.Image = CType(resources.GetObject("BindingNavigatorMoveFirstItem.Image"), System.Drawing.Image)
+        Me.BindingNavigatorMoveFirstItem.Name = "BindingNavigatorMoveFirstItem"
+        Me.BindingNavigatorMoveFirstItem.RightToLeftAutoMirrorImage = True
+        Me.BindingNavigatorMoveFirstItem.Size = New System.Drawing.Size(23, 22)
+        Me.BindingNavigatorMoveFirstItem.Text = "Move first"
+        '
+        'BindingNavigatorMovePreviousItem
+        '
+        Me.BindingNavigatorMovePreviousItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BindingNavigatorMovePreviousItem.Image = CType(resources.GetObject("BindingNavigatorMovePreviousItem.Image"), System.Drawing.Image)
+        Me.BindingNavigatorMovePreviousItem.Name = "BindingNavigatorMovePreviousItem"
+        Me.BindingNavigatorMovePreviousItem.RightToLeftAutoMirrorImage = True
+        Me.BindingNavigatorMovePreviousItem.Size = New System.Drawing.Size(23, 22)
+        Me.BindingNavigatorMovePreviousItem.Text = "Move previous"
+        '
+        'BindingNavigatorSeparator
+        '
+        Me.BindingNavigatorSeparator.Name = "BindingNavigatorSeparator"
+        Me.BindingNavigatorSeparator.Size = New System.Drawing.Size(6, 25)
+        '
+        'BindingNavigatorPositionItem
+        '
+        Me.BindingNavigatorPositionItem.AccessibleName = "Position"
+        Me.BindingNavigatorPositionItem.AutoSize = False
+        Me.BindingNavigatorPositionItem.Name = "BindingNavigatorPositionItem"
+        Me.BindingNavigatorPositionItem.Size = New System.Drawing.Size(50, 23)
+        Me.BindingNavigatorPositionItem.Text = "0"
+        Me.BindingNavigatorPositionItem.ToolTipText = "Current position"
+        '
+        'BindingNavigatorSeparator1
+        '
+        Me.BindingNavigatorSeparator1.Name = "BindingNavigatorSeparator1"
+        Me.BindingNavigatorSeparator1.Size = New System.Drawing.Size(6, 25)
+        '
+        'BindingNavigatorMoveNextItem
+        '
+        Me.BindingNavigatorMoveNextItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BindingNavigatorMoveNextItem.Image = CType(resources.GetObject("BindingNavigatorMoveNextItem.Image"), System.Drawing.Image)
+        Me.BindingNavigatorMoveNextItem.Name = "BindingNavigatorMoveNextItem"
+        Me.BindingNavigatorMoveNextItem.RightToLeftAutoMirrorImage = True
+        Me.BindingNavigatorMoveNextItem.Size = New System.Drawing.Size(23, 22)
+        Me.BindingNavigatorMoveNextItem.Text = "Move next"
+        '
+        'BindingNavigatorMoveLastItem
+        '
+        Me.BindingNavigatorMoveLastItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BindingNavigatorMoveLastItem.Image = CType(resources.GetObject("BindingNavigatorMoveLastItem.Image"), System.Drawing.Image)
+        Me.BindingNavigatorMoveLastItem.Name = "BindingNavigatorMoveLastItem"
+        Me.BindingNavigatorMoveLastItem.RightToLeftAutoMirrorImage = True
+        Me.BindingNavigatorMoveLastItem.Size = New System.Drawing.Size(23, 22)
+        Me.BindingNavigatorMoveLastItem.Text = "Move last"
+        '
+        'BindingNavigatorSeparator2
+        '
+        Me.BindingNavigatorSeparator2.Name = "BindingNavigatorSeparator2"
+        Me.BindingNavigatorSeparator2.Size = New System.Drawing.Size(6, 25)
+        '
+        'SaveToolStripButton
+        '
+        Me.SaveToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.SaveToolStripButton.Image = CType(resources.GetObject("SaveToolStripButton.Image"), System.Drawing.Image)
+        Me.SaveToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.SaveToolStripButton.Name = "SaveToolStripButton"
+        Me.SaveToolStripButton.Size = New System.Drawing.Size(23, 22)
+        Me.SaveToolStripButton.Text = "&Save"
+        '
+        'TextID
+        '
+        Me.TextID.Location = New System.Drawing.Point(261, 139)
+        Me.TextID.Margin = New System.Windows.Forms.Padding(6, 6, 6, 6)
+        Me.TextID.Name = "TextID"
+        Me.TextID.ReadOnly = True
+        Me.TextID.Size = New System.Drawing.Size(96, 29)
+        Me.TextID.TabIndex = 57
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(197, 139)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 24)
+        Me.Label1.TabIndex = 56
+        Me.Label1.Text = "รหัส"
+        '
+        'Label11
+        '
+        Me.Label11.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Label11.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Label11.Location = New System.Drawing.Point(0, 83)
+        Me.Label11.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(1008, 4)
+        Me.Label11.TabIndex = 55
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.Transparent
+        Me.Panel1.Controls.Add(Me.Label12)
+        Me.Panel1.Controls.Add(Me.ButtonOK)
+        Me.Panel1.Controls.Add(Me.Label10)
+        Me.Panel1.Controls.Add(Me.TextSearch)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Margin = New System.Windows.Forms.Padding(6, 6, 6, 6)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(1008, 83)
+        Me.Panel1.TabIndex = 54
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Label12.ForeColor = System.Drawing.Color.Brown
+        Me.Label12.Location = New System.Drawing.Point(58, 24)
+        Me.Label12.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(222, 31)
+        Me.Label12.TabIndex = 23
+        Me.Label12.Text = "จัดการข้อมูลผู้บริจาค"
+        '
+        'ButtonOK
+        '
+        Me.ButtonOK.Location = New System.Drawing.Point(859, 15)
+        Me.ButtonOK.Margin = New System.Windows.Forms.Padding(6, 6, 6, 6)
+        Me.ButtonOK.Name = "ButtonOK"
+        Me.ButtonOK.Size = New System.Drawing.Size(105, 50)
+        Me.ButtonOK.TabIndex = 22
+        Me.ButtonOK.Text = "ตกลง"
+        Me.ButtonOK.UseVisualStyleBackColor = True
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(438, 24)
+        Me.Label10.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(86, 24)
+        Me.Label10.TabIndex = 20
+        Me.Label10.Text = "ค้นหา(ชื่อ)"
+        '
+        'TextSearch
+        '
+        Me.TextSearch.Location = New System.Drawing.Point(555, 25)
+        Me.TextSearch.Margin = New System.Windows.Forms.Padding(6, 6, 6, 6)
+        Me.TextSearch.Name = "TextSearch"
+        Me.TextSearch.Size = New System.Drawing.Size(277, 29)
+        Me.TextSearch.TabIndex = 21
+        '
+        'From_Customers
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(11.0!, 24.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1008, 729)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.TextAddress)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.TextPhone)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TextName)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.BindingNavigator1)
+        Me.Controls.Add(Me.TextID)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Panel1)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(6, 6, 6, 6)
+        Me.Name = "From_Customers"
+        Me.Text = "From_Customers"
+        CType(Me.BindingNavigator1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.BindingNavigator1.ResumeLayout(False)
+        Me.BindingNavigator1.PerformLayout()
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label8 As Label
+    Friend WithEvents TextAddress As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents TextPhone As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextName As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents BindingNavigator1 As BindingNavigator
+    Friend WithEvents BindingNavigatorAddNewItem As ToolStripButton
+    Friend WithEvents BindingNavigatorCountItem As ToolStripLabel
+    Friend WithEvents BindingNavigatorDeleteItem As ToolStripButton
+    Friend WithEvents BindingNavigatorMoveFirstItem As ToolStripButton
+    Friend WithEvents BindingNavigatorMovePreviousItem As ToolStripButton
+    Friend WithEvents BindingNavigatorSeparator As ToolStripSeparator
+    Friend WithEvents BindingNavigatorPositionItem As ToolStripTextBox
+    Friend WithEvents BindingNavigatorSeparator1 As ToolStripSeparator
+    Friend WithEvents BindingNavigatorMoveNextItem As ToolStripButton
+    Friend WithEvents BindingNavigatorMoveLastItem As ToolStripButton
+    Friend WithEvents BindingNavigatorSeparator2 As ToolStripSeparator
+    Friend WithEvents SaveToolStripButton As ToolStripButton
+    Friend WithEvents TextID As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label12 As Label
+    Friend WithEvents ButtonOK As Button
+    Friend WithEvents Label10 As Label
+    Friend WithEvents TextSearch As TextBox
+End Class
---
+++ Form_Master_files/From_Customers.vb
@@ -0,0 +1,141 @@
+﻿Imports System.Data
+Imports System.Data.SqlClient
+
+Public Class From_Customers
+    Private Sub From_Customers_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If connection.State = ConnectionState.Closed Then
+            connection.Open()
+        End If
+
+        BindingNavigator1.DeleteItem = Nothing  '-- Manual Edit
+        BindingData()
+    End Sub
+    Private Sub BindingData(Optional cmd As SqlCommand = Nothing)
+        '-- ก่อนการ Binding ให้เคลียร์ข้อมูลและการ Binding เดิมเพื่อป้องกันข้อผิดพลาด
+        Dim tbx As TextBox
+        For Each ctrl As Control In Me.Controls
+            If TypeOf ctrl Is TextBox Then
+                tbx = CType(ctrl, TextBox)
+                tbx.DataBindings.Clear()
+                tbx.Text = ""
+            End If
+        Next
+        '-- ถ้าไม่มีออบเจ็กต์ SqlCommand ส่งเข้ามาให้อ่านข้อมูลทั้งหมด แต่ถ้ามีให้ใช้ออบเจ็กต์นั้น
+        If cmd Is Nothing Then
+            command.CommandText = "SELECT * FROM Customers"
+        Else
+            command = cmd
+        End If
+
+        adapter = New SqlDataAdapter(command)
+        dataSt = New DataSet()
+        adapter.Fill(dataSt, "Customers")
+
+        bindingSrc = New BindingSource(dataSt, "Customers")
+        TextID.DataBindings.Add("Text", bindingSrc, "CustomerID")
+        TextName.DataBindings.Add("Text", bindingSrc, "CustomerName")
+        TextPhone.DataBindings.Add("Text", bindingSrc, "Phone")
+        TextAddress.DataBindings.Add("Text", bindingSrc, "Address")
+        BindingNavigator1.BindingSource = bindingSrc
+        CreateAutoComplete()
+    End Sub
+
+    Private Sub SaveToolStripButton_Click(sender As Object, e As EventArgs) Handles SaveToolStripButton.Click
+        If TextID.Text = "" Then
+            InsertData()
+        Else
+            UpdateData()
+        End If
+    End Sub
+
+    Private Sub InsertData()
+        sql = "INSERT INTO Customers(CustomerName, Phone , Address) 
+               VALUES(@n, @p, @a)"
+
+        command.CommandText = sql
+        command.Parameters.Clear()
+        command.Parameters.AddWithValue("n", TextName.Text)
+        command.Parameters.AddWithValue("p", TextPhone.Text)
+        command.Parameters.AddWithValue("a", TextAddress.Text)
+
+        Dim r As Integer = command.ExecuteNonQuery()
+        If r = -1 Then
+            MessageBox.Show("เกิดข้อผิดพลาด ไม่สามารถเพิ่มข้อมูลได้")
+        Else
+            MessageBox.Show("บันทึกข้อมูลแล้ว")
+            BindingData()
+        End If
+    End Sub
+
+    Private Sub UpdateData()
+        sql = "UPDATE Customers SET CustomerName = @n, Phone = @p,  Address = @a 
+               WHERE CustomerID = @i"
+
+        command.CommandText = sql
+        command.Parameters.Clear()
+        command.Parameters.AddWithValue("n", TextName.Text)
+        command.Parameters.AddWithValue("p", TextPhone.Text)
+        command.Parameters.AddWithValue("a", TextAddress.Text)
+        command.Parameters.AddWithValue("i", TextID.Text)
+
+        Dim r As Integer = command.ExecuteNonQuery()
+        If r = -1 Then
+            MessageBox.Show("เกิดข้อผิดพลาด ไม่สามารถแก้ไขข้อมูลได้")
+        Else
+            MessageBox.Show("ข้อมูลได้รับการแก้ไขแล้ว")
+            BindingData()
+        End If
+    End Sub
+
+    Private Sub BindingNavigatorDeleteItem_Click(sender As Object, e As EventArgs) Handles BindingNavigatorDeleteItem.Click
+        Dim result As DialogResult =
+        MessageBox.Show("ท่านต้องการลบข้อมูลลูกค้ารายนี้จริงหรือไม่", "ยืนยันการลบ",
+                         MessageBoxButtons.OKCancel)
+
+        If result = DialogResult.Cancel Then
+            Exit Sub
+        End If
+
+        sql = "DELETE FROM Customers WHERE CustomerID = @id"
+        command.CommandText = sql
+        command.Parameters.Clear()
+        command.Parameters.AddWithValue("id", TextID.Text)
+        Dim r As Integer = command.ExecuteNonQuery()
+        If r = -1 Then
+            MessageBox.Show("เกิดข้อผิดพลาด ไม่สามารถลบข้อมูลได้")
+        Else
+            MessageBox.Show("ข้อมูลถูกลบแล้ว")
+            BindingData()
+        End If
+    End Sub
+
+    '-- สร้าง AutoComplete สำหรับช่องค้นหา
+    Private Sub CreateAutoComplete()
+        sql = "SELECT CustomerName FROM Customers"
+        command.CommandText = sql
+        reader = command.ExecuteReader()
+        Dim autoComp As New AutoCompleteStringCollection()
+        While reader.Read()
+            autoComp.Add(reader("CustomerName"))
+        End While
+        reader.Close()
+        TextSearch.AutoCompleteMode = AutoCompleteMode.Suggest
+        TextSearch.AutoCompleteSource = AutoCompleteSource.CustomSource
+        TextSearch.AutoCompleteCustomSource = autoComp
+    End Sub
+
+    '-- ค้นหาข้อมูล
+    Private Sub ButtonOK_Click(sender As Object, e As EventArgs) Handles ButtonOK.Click
+        If String.IsNullOrEmpty(TextSearch.Text) Then
+            BindingData()
+            Exit Sub
+        End If
+
+        sql = "SELECT * FROM Customers WHERE CustomerName LIKE '%' + @n + '%'"
+        command.CommandText = sql
+        command.Parameters.Clear()
+        command.Parameters.AddWithValue("n", TextSearch.Text)
+        BindingData(command)
+    End Sub
+
+End Class
