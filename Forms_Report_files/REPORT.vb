f416dff6edf2af4112289e5786ea3e077f822081
lockheath <34006723+lockheath@users.noreply.github.com>
2018-04-17 17:43:50 +0700

    เพิ่ม Report POS แบบต้องค้นหา
    เพิ่ม Report POS แบบต้องค้นหาโดยปุ่ม Search

---
+++ Forms_Report_files/REPORT.vb
@@ -0,0 +1,16 @@
+﻿Imports CrystalDecisions.CrystalReports.Engine
+'เพิ่มการใช้งาน CrystalReport
+Imports System.Data.SqlClient 'บอกว่าใช้งาน SQL server
+
+
+Public Class REPORT
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim rpt As New ReportDocument
+        Dim directory As String = My.Application.Info.DirectoryPath
+        rpt.Load("C:\MYPROJECT\ProjectRK\Forms_Report_files\CrystalReport01.rpt") 'ที่อยู่ของไฟล์ Report
+        rpt.SetParameterValue("ORDERID", Me.TextBox1.Text) 'ให้เอาค่าจาก txtSearch มา 
+        'ORDERID ให้ไปใส่ใน Parametter ORDERID ที่เราสร้างไว้ใน Cystalreport
+        Me.CrystalReportViewer1.ReportSource = rpt
+        Me.CrystalReportViewer1.Refresh()
+    End Sub
+End Class
